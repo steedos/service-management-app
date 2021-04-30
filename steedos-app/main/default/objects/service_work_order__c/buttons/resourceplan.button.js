6142784ee8b1a31f533d6eb526adf7fe9f9e33a2
marchtian <tianqi@hotoa.com>
2021-04-30 09:44:38 +0800

    增加2个示意按钮

---
+++ steedos-app/main/default/objects/service_work_order__c/buttons/resourceplan.button.js
@@ -0,0 +1,8 @@
+module.exports = {
+    resourceplan: function(object_name, record_id, fields) {
+        alert('待扩充功能。');
+    },
+    resourceplanVisible: function() {
+        return true;
+    }
+}
